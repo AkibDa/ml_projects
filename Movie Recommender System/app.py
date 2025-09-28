46153b482fb0f1dbad5be600cfa42d314646814b
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-09-28 16:00:31 +0530

    middle stage

---
+++ Movie Recommender System/app.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import pickle
+import pandas as pd
+
+similarity = pickle.load(open('similarity.pkl', 'rb'))
+
+def recommend(movie):
+  movie_index = movies[movies['title'] == movie].index[0]
+  distances = similarity[movie_index]
+  movie_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+
+  recommended_movies = []
+  for i in movie_list:
+    recommended_movies.append(movies.iloc[i[0]].title)
+
+  st.subheader("Recommended Movies:")
+  for rec_movie in recommended_movies:
+    st.write(rec_movie)
+
+movies_dict = pickle.load(open('movie_dict.pkl', 'rb'))
+movies = pd.DataFrame(movies_dict)
+
+st.title("Movie Recommendation System 🎬")
+
+
+selected_movie_name = st.selectbox(
+  'Select a movie you like: ',
+  movies['title'].values
+)
+
+
+if st.button('Recommend'):
+  st.write('You selected: ', selected_movie_name)
+  recommend(selected_movie_name)
