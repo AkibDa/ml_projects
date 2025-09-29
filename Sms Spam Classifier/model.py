4aab26df0cc6f4cf1d4b898594c4d9e18294c031
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-09-29 15:16:58 +0530

    built the model

--- Sms Spam Classifier/model.py
+++ Sms Spam Classifier/model.py
@@ -12,7 +12,8 @@ df = df.dropna()
 df = df.drop_duplicates(keep='first')
 
 import nltk, string
-nltk.download('punkt')
+
+nltk.download('punkt_tab')
 
 df['num_characters'] = df['text'].apply(len)
 df['num_words'] = df['text'].apply(lambda x: len(nltk.word_tokenize(x)))
@@ -45,3 +46,55 @@ def transform_text(text):
   return " ".join(y)
 
 df['transformed_text'] = df['text'].apply(transform_text)
+
+# Feature Engineering
+
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+
+cv = CountVectorizer()
+tfidf = TfidfVectorizer()
+
+X = tfidf.fit_transform(df['transformed_text']).toarray()
+y = df['label'].values
+
+# Model Building
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=2)
+
+from sklearn.naive_bayes import MultinomialNB, GaussianNB, BernoulliNB
+
+gnb = GaussianNB()
+mnb = MultinomialNB()
+bnb = BernoulliNB()
+
+gnb.fit(X_train, y_train)
+mnb.fit(X_train, y_train)
+bnb.fit(X_train, y_train)
+
+# Model Evaluation
+
+from sklearn.metrics import accuracy_score, confusion_matrix, precision_score
+
+y_pred1 = gnb.predict(X_test)
+y_pred2 = mnb.predict(X_test)
+y_pred3 = bnb.predict(X_test)
+
+print("Gaussian Naive Bayes")
+print(accuracy_score(y_test, y_pred1))
+print(confusion_matrix(y_test, y_pred1))
+print(precision_score(y_test, y_pred1))
+print("\n")
+
+print("Multinomial Naive Bayes")
+print(accuracy_score(y_test, y_pred2))
+print(confusion_matrix(y_test, y_pred2))
+print(precision_score(y_test, y_pred2))
+print("\n")
+
+print("Bernoulli Naive Bayes")
+print(accuracy_score(y_test, y_pred3))
+print(confusion_matrix(y_test, y_pred3))
+print(precision_score(y_test, y_pred3))
+print("\n")
